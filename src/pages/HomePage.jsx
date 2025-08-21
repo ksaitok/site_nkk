d073253fe680c15fe2c7249edbb3a2f1360f5a6e
kz <kzeroum@gmail.com>
2025-08-21 21:39:08 +0900

    fix: remove duplicated menu

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,16 @@
 
 import React from 'react';
-import Header from '../components/Header';
 import Hero from '../components/Hero';
-import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
     <div>
-      <Header />
       <Hero />
       <main style={{ padding: '2rem' }}>
         <h2>Our Services</h2>
         <p>We offer a wide range of demolition services to meet your needs.</p>
         {/* Add more content here */}
       </main>
-      <Footer />
     </div>
   );
 };
